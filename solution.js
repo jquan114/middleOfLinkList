402d86f94dc00589e98532d3d4dad29374efa9a1
JQUAN COLLINS <104362301+jquan114@users.noreply.github.com>
2023-01-12 14:53:12 -0500

    Create solution.js

---
+++ solution.js
@@ -0,0 +1,12 @@
+Given the head of a singly linked list, return the middle node of the linked list.
+
+var middleNode = function(head) {
+     let slow = head;
+    let fast = head;
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    return slow;
+    
+};
